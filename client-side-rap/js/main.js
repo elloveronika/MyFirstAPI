f78820c760c90919f36fde4fa246ff5f87133a19
elloveronika <ello.veronika92@gmail.com>
2021-04-02 17:02:33 -0700

    FirstAPI

---
+++ client-side-rap/js/main.js
@@ -0,0 +1,14 @@
+document.querySelector('button').addEventListener('click' , getRapName)
+
+async function getRapName(){
+    const rapName = document.querySelector('input').value
+    try{// this is our request to connect to the server
+    const res = await fetch(`http://localhost:8000/api/rappers/${rapName}`)//once isgets data we are going to console log it
+    const data = await res.json() // the promise is pending until we get get the awai
+        document.querySelector('h2').innerText = data.birthName
+        document.querySelector('h3').innerText = data.birthLocation
+    console.log(data)
+    }catch(err){
+        console.log(err)
+    }
+}
---
+++ server.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const { response } = require('express')
+const app = express()
+const cors = require('cors')// download the cors package and now use it
+const PORT = 8000
+
+
+
+app.use(cors())
+app.use(express.static(__dirname + '/clie'))
+
+let rappers = { 
+    'shaheed': {
+        'age': 50,
+        'birthName': 'Ali Shaheed Muhammad',
+        'birthLocation': 'Bedford-Stuyvesant, New York, NY'
+    },
+    'phife dawg': {
+        'age': 50,
+        'birthName': 'Malik Izaak Taylor',
+        'birthLocation': 'St. Albans, New York, NY'
+
+    },
+    'q-tip': {
+        'age': 50,
+        'birthName': 'Kamaal Ibn Fareed',
+        'birthLocation': 'Harlem, New York, NY'
+    },
+    'jarobi': {
+        'age': 49,
+        'birthName': 'Jarobi White',
+        'birthLocation': 'St, Albans, New York, NY'
+    },
+    'unknown': {
+        'age': 1,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown'
+    }
+}
+
+app.get('/', (request, response) => {
+    response.sendFile(__dirname + '/index.html')//wherever the server file is look for the html file
+})
+
+app.get('/api/rappers/:rapperName', (request, response) => {
+    const rapName = request.params.rapperName.toLowerCase()
+    console.log(rapName)
+    if(rappers[rapName]){
+        response.json(rappers[rapName])
+    }else{
+        response.json(rappers['jarobi'])
+    }
+    
+})
+
+app.listen(PORT, () => {
+    console.log(`Server runninng on ${PORT}`)
+})
